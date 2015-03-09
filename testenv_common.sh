1ae733e26dfe1c384c011e4707b14090b641fcad
Dima Kuznetsov <dkuznets@redhat.com>
2015-03-09 15:05:03 +0200

    Add --no-restore to ovirt snapshot
    Signed-off-by: Dima Kuznetsov <dkuznets@redhat.com>

--- testenv_common.sh
+++ testenv_common.sh
@@ -55,6 +55,6 @@ testenv_run () {
 
 	# Start testing
 	testenvcli ovirt runtest $OVIRT_CONTRIB/test_scenarios/bootstrap.py
-	testenvcli ovirt snapshot ovirt-clean
+	testenvcli ovirt snapshot --no-restore ovirt-clean
 	testenvcli ovirt runtest $OVIRT_CONTRIB/test_scenarios/basic_sanity.py
 }
