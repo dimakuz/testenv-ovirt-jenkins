9f688039eb71fad13b90029d35064aee8d71b354
Dima Kuznetsov <dkuznets@redhat.com>
2015-05-21 18:43:39 +0300

    Remove redundant variables
    Signed-off-by: Dima Kuznetsov <dkuznets@redhat.com>

--- testenv_master_centos7.sh
+++ testenv_master_centos7.sh
@@ -2,7 +2,6 @@
 source $WORKSPACE/testenv-ovirt-jenkins/testenv_common.sh
 
 VIRT_CONFIG=$OVIRT_CONTRIB/config/virt/centos7.json
-DEPLOY_SCRIPTS=$OVIRT_CONTRIB/config/deploy/scripts.json
 ANSWER_FILE=$OVIRT_CONTRIB/config/answer-files/el7_master.conf
 
 testenv_run
--- testenv_master_el6.sh
+++ testenv_master_el6.sh
@@ -2,7 +2,6 @@
 source $WORKSPACE/testenv-ovirt-jenkins/testenv_common.sh
 
 VIRT_CONFIG=$OVIRT_CONTRIB/config/virt/centos6.json
-DEPLOY_SCRIPTS=$OVIRT_CONTRIB/config/deploy/scripts.json
 ANSWER_FILE=$OVIRT_CONTRIB/config/answer-files/el6_master.conf
 
 testenv_run
